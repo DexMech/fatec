a9808cbec60fc5ae8717e0dc3a103b66c976aaec
dex <alex.junior.carlos23@gmail.com>
2015-03-14 21:22:28 -0300

    adicionado um botao teste

--- ProjetoCristina/src/controller/TelaRomaneio.java
+++ ProjetoCristina/src/controller/TelaRomaneio.java
@@ -6,10 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
 
 public class TelaRomaneio extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,8 +37,13 @@ public class TelaRomaneio extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(31, 52, 114, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
 	}
 
 }
