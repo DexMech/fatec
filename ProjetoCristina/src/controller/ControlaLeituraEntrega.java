ce1ab7624deb559d7b86d01c57fd2155e8da3b70
dexMech <alex.junior.carlos23@gmail.com>
2015-04-15 00:50:57 -0300

    mudancas nas classes de consulta de entrega e leitura

--- ProjetoCristina/src/controller/ControlaLeituraEntrega.java
+++ ProjetoCristina/src/controller/ControlaLeituraEntrega.java
@@ -1,7 +1,70 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import view.ConsultaEntrega;
+
 public class ControlaLeituraEntrega {
-
-
-
+private ConsultaEntrega consulta = new ConsultaEntrega();
+private JTextField s;
+public void sair(){
+	
+	
+	int resp = 	JOptionPane.showConfirmDialog(null, "deseja mesmo sair","Saindo",JOptionPane.INFORMATION_MESSAGE);
+	if(resp==0){
+		
+		System.exit(0);
+		
+	}
 }
+
+
+public void busca(JTable table,JTextField cliente){
+	System.out.println(consulta.getTxtCliente());
+/*
+			DefaultTableModel modelo = (DefaultTableModel) table.getModel();
+			String linha;
+		
+			try {
+				BufferedReader le = new BufferedReader(new FileReader("entrega.txt"));
+		while(le.ready()){
+				linha = le.readLine();
+				if(linha.contains(cliente.getText())){
+				Object[] objeto = linha.split(";");
+				modelo.addRow(objeto);
+				}
+				
+				}
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
+			}
+			*/
+		}
+	
+	
+
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
--- ProjetoCristina/src/view/ConsultaEntrega.java
+++ ProjetoCristina/src/view/ConsultaEntrega.java
@@ -13,13 +13,29 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 
+import controller.ControlaLeituraEntrega;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Color;
+
+import javax.swing.JTextField;
 
 public class ConsultaEntrega extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table;
+	public JTable table;
+	private JLabel labelControl;
+	private JTextField txtCliente;
+	public String getTxtCliente() {
+		return txtCliente.getText();
+	}
+
+	private JTextField txtdata;
+
 
 	/**
 	 * Launch the application.
@@ -54,13 +70,21 @@ public class ConsultaEntrega extends JFrame {
 		contentPane.add(scrollPane);
 		
 		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Regi\u00E3o", "Cliente", "Caminh\u00E3o", "Data"
+			}
+		));
 		scrollPane.setViewportView(table);
 		
 		JButton btnSair = new JButton("Sair");
 		btnSair.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				
+			new ControlaLeituraEntrega().sair();
+			
 			}
 		});
 		btnSair.setBounds(572, 385, 117, 25);
@@ -69,14 +93,6 @@ public class ConsultaEntrega extends JFrame {
 		JButton btnExportarCvs = new JButton("Exportar cvs");
 		btnExportarCvs.setBounds(54, 385, 117, 25);
 		contentPane.add(btnExportarCvs);
-		
-		JComboBox comboCliente = new JComboBox();
-		comboCliente.setBounds(113, 51, 117, 24);
-		contentPane.add(comboCliente);
-		
-		JComboBox comboData = new JComboBox();
-		comboData.setBounds(413, 51, 116, 24);
-		contentPane.add(comboData);
 		
 		JLabel labelCliente = new JLabel("Cliente");
 		labelCliente.setBounds(51, 38, 57, 50);
@@ -87,7 +103,31 @@ public class ConsultaEntrega extends JFrame {
 		contentPane.add(labelData);
 		
 		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//System.out.println(getTxtCliente());
+				ControlaLeituraEntrega c = new ControlaLeituraEntrega();
+				
+				c.busca(table,txtCliente);
+				
+			}
+		});
 		btnBuscar.setBounds(572, 51, 117, 25);
 		contentPane.add(btnBuscar);
+		
+		JLabel labelControl = new JLabel("");
+		labelControl.setForeground(Color.RED);
+		labelControl.setBounds(447, 6, 60, 15);
+		contentPane.add(labelControl);
+		
+		txtCliente = new JTextField();
+		txtCliente.setBounds(100, 50, 122, 27);
+		contentPane.add(txtCliente);
+		txtCliente.setColumns(10);
+		
+		txtdata = new JTextField();
+		txtdata.setBounds(397, 50, 122, 27);
+		contentPane.add(txtdata);
+		txtdata.setColumns(10);
 	}
 }
